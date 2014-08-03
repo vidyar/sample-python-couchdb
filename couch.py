a9ade28217a5cc2934f1845f5541949f3cd87625
Vidya Raghavan <vidya@shippable.com>
2014-08-03 14:02:48 +0530

    Revert "Update test.py"

--- couch.py
+++ couch.py
@@ -14,8 +14,7 @@ class Couch():
             {"name": "Lakshmi"},
             {"name": "Ganesha"},
             {"name": "Krishna"},
-            {"name": "Murugan"},
-            {"name": "Shiva"}
+            {"name": "Murugan"}
         ]
         self.db.save_docs(things)
 
